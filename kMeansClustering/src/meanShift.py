371a41281f1dad47a2438f1dfac22deef8b4363f
Ronald Fitzgerald <Ronald.L.Fitzgerald@aero.org>
2014-10-08 16:26:07 -0700

    Did a bit of tidying up.

--- kMeansClustering/src/meanShift.py
+++ kMeansClustering/src/meanShift.py
@@ -7,4 +7,4 @@ def meanShift(filename):
 
     img = cv2.imread(filename)
     (segmented_image, labels_image, number_regions) = pms.segment(img, spatial_radius=6, range_radius=4.5, min_density=50)
-    cv2.imwrite('segmented_%s' % filename, segmented_image)
+    return segmented_image
