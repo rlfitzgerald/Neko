1f063cc2e2fb29cb69e3baf9e5dc4aa170b79402
Andrew <andrew@devbox.aero.org>
2014-10-29 14:49:35 -0700

    Restarted training with a proper taining set.

--- haarTraining/src/haar_training.sh
+++ haarTraining/src/haar_training.sh
@@ -3,24 +3,24 @@
 find ./images/positive/ -type f -name '*.png' > positives.dat
 find ./images/negative/ -type f -name '*.png' > negatives.dat
 
-[ -f collection.dat ] && rm -f collection.dat
-
-# get width and height of each image, output to collection.dat file
-for f in $(cat positives.dat); do
-    dim=$(identify "$f" | cut -d ' ' -f 3 | tr 'x' ' ')
-    echo "$f 1 0 0 $dim"
-done > collection.dat
-
-#NUMPOS=$(wc -l collection.dat)
 NUMPOS=$(wc -l < collection.dat)
 NUMNEG=$(wc -l < negatives.dat)
 
-[ -f samples.vec ] && rm -f samples.vec
+[ -d samples ] && rm -r samples
+mkdir samples
 
 # create samples.vec file needed by classifier
-opencv_createsamples -info collection.dat -bgcolor 0 -bgthresh 0 -vec samples.vec -num $NUMPOS -w 20 -h 20
+perl createtestsamples.pl positives.dat negatives.dat samples 1500 "opencv_createsamples -bgcolor 0 -bgthresh 0 -maxxangle 1.1 -maxyangle 1.1 -maxzangle 0.5 -maxidev 40 -w 20 -h 20"
+find ./samples -name '*.vec' > samples.txt
+./mergevec samples.txt samples.vec
+
+
+[ -d classifier ] && rm -r classifier
+mkdir classifier
 
 # train classifier to detect positive images
 # for precalculation you can set the amount of memory to use
 # set sample size of positive images, 20 x 20 seems to work well
-opencv_traincascade -data classifier2 -featureType HAAR -vec samples.vec -bg negatives.dat -numPos $NUMPOS -numNeg $NUMNEG -numStages 20 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -minHitRate 0.7 -maxFalseAlarmRate 0.5 -mode ALL -w 20 -h 20
+#opencv_traincascade -data classifier -featureType HAAR -vec samples.vec -bg negatives.dat -numPos $NUMPOS -numNeg $NUMNEG -numStages 20 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -minHitRate 0.7 -maxFalseAlarmRate 0.5 -mode ALL -w 20 -h 20
+#opencv_traincascade -data classifier -vec samples.vec -bg negatives.dat -numStages 20 -precalcValBufSize 6000 -precalcIdxBufSize 6000 -minHitRate 0.7 -maxFalseAlarmRate 0.5 -mode ALL -w 20 -h 20
+opencv_traincascade -data classifier -vec samples.vec -bg negatives.dat -numStages 20 -minHitRate 0.8 -maxFalseAlarmRate 0.5 -numPos 700 -numNeg 200 -w 20 -h 20 -mode ALL -precalcValBufSize 1024 -precalcIdxBufSize 1024
